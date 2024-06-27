e05189012a32d491ba3e9e041e5e6828e98a2462
MauricioCam <camardellamauricio@gmail.com>
2024-06-27 18:57:54 -0300

    .

--- pages/Tienda.py
+++ pages/Tienda.py
@@ -3,7 +3,6 @@ import numpy as np
 import streamlit as st
 import os
 from st_pages import show_pages_from_config, add_page_title
-import streamlit as st
 import streamlit_shadcn_ui as ui
 from streamlit_card import card
 import base64
@@ -30,7 +29,6 @@ def mostrar_producto(imagen, titulo, descripcion, precio, index):
         st.header(titulo)
         st.write(descripcion)
         st.subheader(f"Precio: {precio}")
-        # Proporciona una clave única para cada botón usando el índice
         if st.button(f'Agregar {titulo} al carrito', on_click=agregar_al_carrito, args=({"titulo": titulo, "precio": precio},), key=f"btn_{index}"):
             st.experimental_rerun()
 
@@ -57,26 +55,8 @@ choice = ui.select(options=["Todos los tonos", "Muy Claro", "Claro", "Medio", "O
 
 index = 0
 
-if choice == "Todos los tonos":
-    with open('Fotos/Portada.jpeg', "rb") as f:
-        data = f.read()
-        encoded = base64.b64encode(data)
-    data = "data:image/png;base64," + encoded.decode("utf-8")
-
-    res = card(
-        title="SKINLY",
-        text="",
-        image=data,
-        styles={
-            "card": {
-                "width": "100%",
-                "height": "150px"
-            }
-        }
-    )
-
-    # Datos de los Bases
-    bases = [
+productos = {
+    "Todos los tonos": [
         {
             "imagen": "Fotos/BASE_MUY_CLARA.png",
             "titulo": "Base Muy Clara",
@@ -106,19 +86,7 @@ if choice == "Todos los tonos":
             "titulo": "Base Muy Oscura",
             "descripcion": "Nuestra Base Muy Oscura ofrece una cobertura completa para tonos de piel muy oscuros, asegurando un look natural y radiante.",
             "precio": "$100"
-        }
-    ]
-
-    # Mostrar productos
-    for base in bases:
-        mostrar_producto(base["imagen"], base["titulo"], base["descripcion"], base["precio"], index)
-        st.markdown("---")
-        index += 1
-    
-    st.header('Correctores')
-
-    # Datos de los Correctores
-    correctores = [
+        },
         {
             "imagen": "Fotos/CORRECTOR_MUY_CLARO.png",
             "titulo": "Corrector Muy Claro",
@@ -148,19 +116,7 @@ if choice == "Todos los tonos":
             "titulo": "Corrector Muy Oscuro",
             "descripcion": "Nuestro Corrector Muy Oscuro ofrece una cobertura completa para tonos de piel muy oscuros, asegurando un look natural y radiante.",
             "precio": "$150"
-        }
-    ]
-
-    # Mostrar productos
-    for corrector in correctores:
-        mostrar_producto(corrector["imagen"], corrector["titulo"], corrector["descripcion"], corrector["precio"], index)
-        st.markdown("---")
-        index += 1
-
-    st.header('Polvos')
-
-    # Datos de los Polvos
-    polvos = [
+        },
         {
             "imagen": "Fotos/POLVO_MUY_CLARO.png",
             "titulo": "Polvo Muy Claro",
@@ -191,36 +147,8 @@ if choice == "Todos los tonos":
             "descripcion": "El Polvo Muy Oscuro fija el maquillaje y controla el brillo en tonos de piel muy oscuros, asegurando un look impecable y natural.",
             "precio": "$150"
         }
-    ]
-
-    # Mostrar productos
-    for polvo in polvos:
-        mostrar_producto(polvo["imagen"], polvo["titulo"], polvo["descripcion"], polvo["precio"], index)
-        st.markdown("---")
-        index += 1
-
-# Código similar para otras opciones (Muy Claro, Claro, Medio, Oscuro, Muy Oscuro)
-
-if choice == "Muy Claro":
-    with open('Fotos/MUY_CLARO.png', "rb") as f:
-        data = f.read()
-        encoded = base64.b64encode(data)
-    data = "data:image/jpeg;base64," + encoded.decode("utf-8")
-
-    res = card(
-        title="Muy Claro",
-        text="",
-        image=data,
-        styles={
-            "card": {
-                "width": "100%",
-                "height": "150px"
-            }
-        }
-    )
-
-    # Datos de los productos Muy Claros
-    muyclaros = [
+    ],
+    "Muy Claro": [
         {
             "imagen": "Fotos/BASE_MUY_CLARA.png",
             "titulo": "Base Muy Clara",
@@ -239,12 +167,89 @@ if choice == "Muy Claro":
             "descripcion": "Fija tu maquillaje con nuestro Polvo Muy Claro, diseñado para tonos de piel porcelana, ofreciendo un acabado mate y suave.",
             "precio": "$150"
         }
+    ],
+    "Claro": [
+        {
+            "imagen": "Fotos/BASE_CLARA.png",
+            "titulo": "Base Clara",
+            "descripcion": "Nuestra Base Clara proporciona una cobertura uniforme y un acabado radiante para pieles claras, manteniendo la frescura durante todo el día.",
+            "precio": "$100"
+        },
+        {
+            "imagen": "Fotos/CORRECTOR_CLARO.png",
+            "titulo": "Corrector Claro",
+            "descripcion": "Nuestro Corrector Claro proporciona una cobertura uniforme y un acabado radiante para pieles claras, manteniendo la frescura durante todo el día.",
+            "precio": "$150"
+        },
+        {
+            "imagen": "Fotos/POLVO_CLARO.png",
+            "titulo": "Polvo Claro",
+            "descripcion": "El Polvo Claro proporciona una fijación perfecta y un acabado natural para pieles claras, manteniendo el maquillaje intacto todo el día.",
+            "precio": "$150"
+        }
+    ],
+    "Medio": [
+        {
+            "imagen": "Fotos/BASE_MEDIA.png",
+            "titulo": "Base Media",
+            "descripcion": "Descubre la perfección con nuestra Base Media, diseñada para tonos de piel medios, ofreciendo una textura suave y cobertura duradera.",
+            "precio": "$100"
+        },
+        {
+            "imagen": "Fotos/CORRECTOR_MEDIO.png",
+            "titulo": "Corrector Medio",
+            "descripcion": "Descubre la perfección con nuestro Corrector Medio, diseñado para tonos de piel medios, ofreciendo una textura suave y cobertura duradera.",
+            "precio": "$150"
+        },
+        {
+            "imagen": "Fotos/POLVO_MEDIO.png",
+            "titulo": "Polvo Medio",
+            "descripcion": "Nuestro Polvo Medio es ideal para tonos de piel medios, ofreciendo un acabado mate y control de brillo durante horas.",
+            "precio": "$150"
+        }
+    ],
+    "Oscuro": [
+        {
+            "imagen": "Fotos/BASE_OSCURA.png",
+            "titulo": "Base Oscura",
+            "descripcion": "La Base Oscura se adapta perfectamente a tonos de piel oscuros, proporcionando una cobertura sin igual y un acabado mate impecable.",
+            "precio": "$100"
+        },
+        {
+            "imagen": "Fotos/CORRECTOR_OSCURO.png",
+            "titulo": "Corrector Oscuro",
+            "descripcion": "El Corrector Oscuro se adapta perfectamente a tonos de piel oscuros, proporcionando una cobertura sin igual y un acabado mate impecable.",
+            "precio": "$150"
+        },
+        {
+            "imagen": "Fotos/POLVO_OSCURO.png",
+            "titulo": "Polvo Oscuro",
+            "descripcion": "Controla el brillo y fija tu maquillaje con nuestro Polvo Oscuro, perfecto para tonos de piel oscuros, proporcionando un acabado suave y uniforme.",
+            "precio": "$150"
+        }
+    ],
+    "Muy Oscuro": [
+        {
+            "imagen": "Fotos/BASE_MUY_OSCURA.png",
+            "titulo": "Base Muy Oscura",
+            "descripcion": "Nuestra Base Muy Oscura ofrece una cobertura completa para tonos de piel muy oscuros, asegurando un look natural y radiante.",
+            "precio": "$100"
+        },
+        {
+            "imagen": "Fotos/CORRECTOR_MUY_OSCURO.png",
+            "titulo": "Corrector Muy Oscuro",
+            "descripcion": "Nuestro Corrector Muy Oscuro ofrece una cobertura completa para tonos de piel muy oscuros, asegurando un look natural y radiante.",
+            "precio": "$150"
+        },
+        {
+            "imagen": "Fotos/POLVO_MUY_OSCURO.png",
+            "titulo": "Polvo Muy Oscuro",
+            "descripcion": "El Polvo Muy Oscuro fija el maquillaje y controla el brillo en tonos de piel muy oscuros, asegurando un look impecable y natural.",
+            "precio": "$150"
+        }
     ]
+}
 
-    # Mostrar productos Muy Claros
-    for producto in muyclaros:
-        mostrar_producto(producto["imagen"], producto["titulo"], producto["descripcion"], producto["precio"], index)
-        st.markdown("---")
-        index += 1
-
-# Repetir la estructura similar para las otras opciones Claro, Medio, Oscuro, Muy Oscuro
+for producto in productos[choice]:
+    mostrar_producto(producto["imagen"], producto["titulo"], producto["descripcion"], producto["precio"], index)
+    index += 1
